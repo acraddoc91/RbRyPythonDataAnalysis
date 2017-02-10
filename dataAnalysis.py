34847c9f3eee62495ee2339b2dd2e84203a66c7c
acraddoc91 <acraddoc@umd.edu>
2017-02-10 17:35:56 -0500

    Updated main window code
    Main window now functions with the new way of processing files

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,23 +4,23 @@ Created on Oct 11, 2016
 @author: Sandy
 '''
 import matplotlib as mpl
+from PyQt5.Qt import QWidget
 mpl.use('Agg')
 from PyQt5.uic import loadUiType
 from PyQt5.QtCore import (QThread, pyqtSignal)
 from dataServer import (requestServer,requestHandler)
 from processing import (shotProcessor,grabImage)
-from PyQt5.QtWidgets import (QListWidgetItem,QTableWidgetItem, QComboBox, QCheckBox,
-    QWidget, QHBoxLayout)
+from PyQt5.QtWidgets import (QListWidgetItem,QTableWidgetItem, QComboBox, QCheckBox,QFileDialog)
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (FigureCanvasQTAgg as FigureCanvas)
 import pandas as pd
 import threading
 import time
 import json
-import gc
+from PyQt5.QtGui import QFont
 
 #This loads the UI we want to use
-uiMainWindow, qMainWindow = loadUiType('testServer.ui')    
+uiMainWindow, qMainWindow = loadUiType('dataAnalysis.ui')    
 
 #This is a server class that will be started as a thread which will sit there and wait for HTTP commands then perform them. It wraps the server itself and allows it to interact with the GUI
 class serverThread(QThread):
@@ -81,6 +81,39 @@ class serverThread(QThread):
             #self.emit(SIGNAL('showData'))
             self.showDataSignal.emit()
 
+#This thread will allow us to asynchonously process files     
+class processorThread(QThread):
+    #This allows us to trigger actions from the main thread
+    trigger = pyqtSignal(int)
+    #This holds any newly processed data till it gets grabbed by the main thread
+    newData = pd.DataFrame()
+    newDataSignal = pyqtSignal()
+    showDataSignal = pyqtSignal()
+    filenames = []
+    methods = []
+    processData = True
+    
+    def __init__(self):
+        QThread.__init__(self)
+        self.threadExiting = False
+    def __del__(self):
+        self.wait()
+        self.threadExiting = True
+    def run(self):
+        #This loop keeps the thread going till we want it to die taking commands from HTTP and performing the appropriate actions
+        while not self.threadExiting:
+            if len(self.filenames) != 0 & self.processData:
+                for file in self.filenames[0]:
+                    self.newData = self.newData.append(shotProcessor(file, self.methods[0]))
+                del self.filenames[0]
+                del self.methods[0]
+                self.processData = False
+                self.newDataSignal.emit()
+    def setFilesToProcess(self,filenames,methods):
+        self.filenames.append(filenames)
+        self.methods.append(methods)
+        print(len(self.filenames))
+
 class Main(qMainWindow,uiMainWindow):
     #This is our main data frame. It will hold all our data, equivalent to the main workspace in Matlab
     data = pd.DataFrame()
@@ -94,6 +127,10 @@ class Main(qMainWindow,uiMainWindow):
         self.servThread = serverThread()
         self.servThread.start()
         
+        #Start processor thread
+        self.procThread = processorThread()
+        self.procThread.start()
+        
         #Setup the data and image figures
         self.dataFig = Figure()
         self.dataAxes = self.dataFig.add_subplot(111)
@@ -106,17 +143,23 @@ class Main(qMainWindow,uiMainWindow):
         self.imageIndexLayout.addWidget(self.imageCanvas)
         
         #Connect everything to their relevant functions
-        #self.connect(self.servThread,SIGNAL('newData'),self.grabNewData)
-        self.servThread.newDataSignal.connect(self.grabNewData)
+        self.procThread.newDataSignal.connect(self.grabNewData)
         self.xAxis.itemClicked.connect(self.plotData)
         self.yAxis.itemClicked.connect(self.plotData)
         self.indexList.itemClicked.connect(self.showImage)
         self.addCut.clicked.connect(self.addCutFunc)
         self.delCut.clicked.connect(self.delCutFunc)
         self.setCuts.clicked.connect(self.setCutsFunc)
+        self.addShotMethod.clicked.connect(self.addShotMethodFunc)
+        self.removeShotMethod.clicked.connect(self.removeShotMethodFunc)
+        self.actionLoad_files.triggered.connect(self.loadFiles)
         
         #Setup cutTable
         self.cutTable.setHorizontalHeaderLabels(['Field','Cut Type','Cut Value','Active'])
+    
+        #Set text size
+        labelFont = QFont('Arial',15)
+        self.label_3.setFont(labelFont)
     
     #Function called to plot data to the graph in tab 1
     def plotData(self):
@@ -132,32 +175,33 @@ class Main(qMainWindow,uiMainWindow):
             
     #Function called to update image in tab 2
     def showImage(self):
-        dummy = self.data[self.data['FileNumber'] == int(str(self.indexList.currentItem().text()))]
+        pass
+        #dummy = self.data[self.data['FileNumber'] == int(str(self.indexList.currentItem().text()))]
         #self.imageFig.clear()
         #self.imageFig = grabImage(dummy['Filename'].values[-1],dummy['FitType'].values[-1])
-        grabImage(dummy['Filename'].values[-1],dummy['FitType'].values[-1],self.imageAxes,self.imageFig)
+        #grabImage(dummy['Filename'].values[-1],dummy['FitType'].values[-1],self.imageAxes,self.imageFig)
         #if hasattr(self, 'imageCanvas'):
         #    self.imageIndexLayout.removeWidget(self.imageCanvas)
         #    self.imageCanvas.close()
         #self.imageCanvas = FigureCanvas(self.imageFig)
         #self.imageIndexLayout.addWidget(self.imageCanvas)
-        self.imageCanvas.draw()
+        #self.imageCanvas.draw()
         
-        self.imageDataTable.clear()
-        self.imageDataTable.setRowCount(len(self.varNames))
-        for i in range(0,len(self.varNames)):
-            self.imageDataTable.setItem(i,0,QTableWidgetItem(str(self.varNames[i])))
-            self.imageDataTable.setItem(i,1,QTableWidgetItem(str(dummy[self.varNames[i]].values[-1])))
-        del dummy
+        #self.imageDataTable.clear()
+        #self.imageDataTable.setRowCount(len(self.varNames))
+        #for i in range(0,len(self.varNames)):
+        #    self.imageDataTable.setItem(i,0,QTableWidgetItem(str(self.varNames[i])))
+        #    self.imageDataTable.setItem(i,1,QTableWidgetItem(str(dummy[self.varNames[i]].values[-1])))
+        #del dummy
         
     #Function called when data is available in the server thread
     def grabNewData(self):
-        self.data = self.data.append(self.servThread.newData,ignore_index=True)
+        self.data = self.data.append(self.procThread.newData,ignore_index=True)
         newNames = [x for x in self.data.columns.values.tolist() if x not in self.varNames]
         for name in newNames:
             self.xAxis.addItem(QListWidgetItem(name))
             self.yAxis.addItem(QListWidgetItem(name))
-        for index in self.servThread.newData['FileNumber']:
+        for index in self.procThread.newData['FileNumber']:
             self.indexList.addItem(QListWidgetItem(str(index)))
         if self.varNames == []:
             self.xAxis.setCurrentRow(0)
@@ -165,8 +209,10 @@ class Main(qMainWindow,uiMainWindow):
             self.indexList.setCurrentRow(0)
             self.plotData()
         self.varNames = self.varNames+newNames
-        self.servThread.newData = pd.DataFrame() 
-        
+        self.procThread.newData = pd.DataFrame() 
+        self.procThread.processData = True
+        print('new data acquired')
+    #Adds a line to the cut table    
     def addCutFunc(self):
         self.cutTable.insertRow(self.cutTable.rowCount())
         numRows = self.cutTable.rowCount()
@@ -180,6 +226,7 @@ class Main(qMainWindow,uiMainWindow):
         self.cutTable.setCellWidget(numRows-1,0,varNameComboBox)
         self.cutTable.setCellWidget(numRows-1,1,cutTypeComboBox)
         self.cutTable.setCellWidget(numRows-1,3,QCheckBox())
+    #Removes line from the cut table
     def delCutFunc(self):
         dummy = self.cutTable.selectionModel()
         delRows = []
@@ -189,8 +236,10 @@ class Main(qMainWindow,uiMainWindow):
         delRows = list(reversed(delRows))
         for row in delRows:
             self.cutTable.removeRow(row)
+    #Replots when the cut table has been adjusted
     def setCutsFunc(self):
         self.plotData()
+    #Returns the truth table for which data points pass the parameters of the cut table
     def grabTruthTable(self):
         rowCount = self.cutTable.rowCount()
         #This should return a truth table that is all true elements
@@ -217,6 +266,31 @@ class Main(qMainWindow,uiMainWindow):
                     self.cutTable.cellWidget(i,3).setCheckState(0)
                     self.cutTable.cellWidget(i,2).item(i,2).setText('Invalid Cut Value')
         return truthTable
+    #Adds a processing method to the shot method table
+    def addShotMethodFunc(self):
+        self.singleShotMethodsTable.insertRow(self.singleShotMethodsTable.rowCount())
+    #Removes a processing method from the shot method table
+    def removeShotMethodFunc(self):
+        dummy = self.singleShotMethodsTable.selectionModel()
+        delRows = []
+        for selectObj in dummy.selectedRows():
+            delRows.append(selectObj.row())
+        del dummy
+        delRows = list(reversed(delRows))
+        for row in delRows:
+            self.singleShotMethodsTable.removeRow(row)
+    def loadFiles(self):
+        dummy = QWidget()
+        filenames = QFileDialog.getOpenFileNames(dummy,'Open File',filter="*.h5")[0]
+        methods = self.getMethodListFromTable()
+        self.procThread.filenames.append(filenames)
+        self.procThread.methods.append(methods)
+    def getMethodListFromTable(self):
+        rowCount = self.singleShotMethodsTable.rowCount()
+        methods = []
+        for i in range(0,rowCount):
+            methods.append(str(self.singleShotMethodsTable.item(i,0).text()))
+        return methods
 if __name__=='__main__':
     import sys
     from PyQt5 import QtWidgets
